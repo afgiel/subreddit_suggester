99118b18775c0a5a27480d1c577bbed5b06dca0e
afgiel <agiel@stanford.edu>
2014-10-27 17:19:15 -0700

    first commit

---
+++ print_top_sp.py
@@ -0,0 +1,18 @@
+import csv
+import os
+import os.path as path
+from collections import Counter
+
+DATA_PATH = '../reddit-top-2.5-million/data/'
+self_counts = Counter()
+for dirpath, dirnames, filenames in os.walk(DATA_PATH):
+  for filename in filenames:
+    with open(path.join(DATA_PATH, filename)) as csvfile:
+      csvreader = csv.reader(csvfile)
+      for row in csvreader:
+        if 'self.' in row[2]:
+          self_counts[filename] += 1
+
+
+for sub, count in self_counts.most_common(100):
+  print sub, count 
