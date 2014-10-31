fddb819658054c6772d8a304ba7dafc29639b4c3
afgiel <agiel@stanford.edu>
2014-10-31 08:11:54 -0700

    added subreddits

---
+++ constants.py
@@ -0,0 +1,14 @@
+subreddits = [
+  "nostupidquestions",
+  "shortscarystories",
+  "showerthoughts",
+  "debatereligion",
+  "confession",
+  "relationship_advice",
+  "unsentletters",
+  "self",
+  "askphilosophy",
+  "shittypoetry",
+  "askmen",
+  "askwomen"
+]
