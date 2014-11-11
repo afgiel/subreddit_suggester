71eeeaf2b3076a8f911692b8ac83ab05a262320a
afgiel <agiel@stanford.edu>
2014-11-10 20:34:30 -0800

    more constructive evaluation info

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,4 +1,4 @@
-
+import constants
 
 def evaluate(des, pred):
   num_right = 0. 
@@ -9,7 +9,29 @@ def evaluate(des, pred):
     if des_i == pred_i: 
       num_right += 1
     else: 
-      num_wrong += 1
-
+      num_wrong += 1 
   accuracy = num_right/(num_right + num_wrong)
-  print 'ACCURACY ===== ' + str(accuracy)
+  print '\tACCURACY: ' + str(accuracy)
+  print '\t======='
+
+  for s in range(len(constants.subreddits)): 
+    tp = 0.
+    fp = 0.
+    fn = 0.
+    for i in range(len(des)):
+      des_i = des[i] 
+      pred_i = pred[i]
+      if des_i == s:
+        if des_i == pred_i:
+          tp += 1
+        else: 
+          fn += 1 
+      elif pred_i == s:
+        fp += 1  
+    precision = tp/(tp + fn)  
+    recall = tp/(tp + fp)
+    print '\tFOR SUBREDDIT ' + constants.subreddits[s] 
+    print '\tPRECISION: ' + str(precision)
+    print '\tRECALL: ' + str(recall)
+    print '\t======='
+  print '\n--------\n'
