e7b6b462334306530bfdbc43fc44c66e79547045
afgiel <agiel@stanford.edu>
2014-11-13 22:27:39 -0800

    added train error

--- src/run_train_test.py
+++ src/run_train_test.py
@@ -22,8 +22,9 @@ def run():
   for subreddit in constants.subreddits:
     sub_all = load_subreddit_data.get_all_posts_and_labels(subreddit) 
     num_posts = len(sub_all)  
-    sub_train = random.sample(sub_all, int(num_posts*.9)) 
-    sub_test = [x for x in sub_all if x not in sub_train]
+    sub_train = random.sample(sub_all, int(num_posts*.05)) 
+    #sub_test = [x for x in sub_all if x not in sub_train]
+    sub_test = random.sample(sub_all, int(num_posts*.01))
     train_set.extend(sub_train)
     test_set.extend(sub_test)
   print 'COUNTING TRAIN SET' 
@@ -60,6 +61,13 @@ def run():
   logistic_regression.fit(train_x, train_y)
   #print '\tSVM'
   #svm.fit(train_x, train_y)
+  print 'EVALUATING TRAIN'
+  nb_predicted_y = naive_bayes.predict(train_x)
+  logres_predicted_y = logistic_regression.predict(train_x) 
+  print '\tNAIVE BAYES'
+  print classification_report(train_y, nb_predicted_y)
+  print '\tLOGISTIC REGRESSION'
+  print classification_report(train_y, logres_predicted_y)
   print 'FEATURIZING TEST SET'  
   test_posts = [x[0] for x in test_set]
   test_titles = [x.title for x in test_posts]
@@ -75,7 +83,7 @@ def run():
   nb_predicted_y = naive_bayes.predict(test_x)
   logres_predicted_y = logistic_regression.predict(test_x) 
   #svm_predicted_y = svm.predict(test_x)
-  print 'EVALUATING'
+  print 'EVALUATING TEST'
   print '\tNAIVE BAYES'
   print classification_report(desired_y, nb_predicted_y)
   print '\tLOGISTIC REGRESSION'
