3f7220529bdaba6af1651404255d5f2fe52c13c7
afgiel <agiel@stanford.edu>
2014-11-17 20:15:38 -0800

    changed tokenize to nltk

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,4 @@
-import json
-
 from nltk.tokenize import word_tokenize
 
 def tokenize(text):
-    return word_tokenize(text)
- 
-def write_json_file(obj, file_path):
-  with open(file_path, 'w') as json_file:
-    json_file.write(json.dumps(obj))
-
-def load_json_file(file_path):
-  with open(file_path, 'r') as json_file:
-    json_str = json_file.readline()
-    return json.loads(json_str)
+    return word_tokenize(text) 
