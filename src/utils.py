ce42c5d7e6ec3588320253e455adc96d029df002
afgiel <agiel@stanford.edu>
2014-11-17 20:15:38 -0800

    changed tokenize to nltk

--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,4 @@
-import string
-import re
-
-import nltk
-from nltk.stem import PorterStemmer
-from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 def tokenize(text):
-    char_from = '[' + string.punctuation + string.whitespace + ']'
-    char_to = ' '
-    words = re.sub(char_from, char_to, text)
-    words = words.lower().split()
-    tokens = []
-    excluded = set([w.encode('utf-8') for w in stopwords.words('english')])
-    stemmer = PorterStemmer()
-    excluded_re = '\\\\|[0-9]|\.[a-z]+'
-    for word in words:
-      if word not in excluded and re.match(excluded_re, word) is None:
-        tokens.append(stemmer.stem(word))
-    return tokens
+    return word_tokenize(text) 
